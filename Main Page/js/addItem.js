619f6c503e19cfa475cb6db61fe3e0af0bb41b56
maria-ma2108455 <ma2108455@qu.edu.qa>
2024-03-28 22:18:55 +0300

    seller add item almost there imagetoUrl

--- Main Page/js/addItem.js
+++ Main Page/js/addItem.js
@@ -1,21 +1,29 @@
 
 
+
+const uploadImage= document.querySelector('#item-image')
 let items=[]
 let users=[]
 items= JSON.parse(localStorage.items)
 users= JSON.parse(localStorage.users)
-const form = document.querySelector('#searchForm')
+const form = document.querySelector('#addItem-form')
 form.addEventListener('submit',addForm)
 function addForm(e){
+    console.log("Form submitted");
     e.preventDefault()
     const item= formToObject(e.target)
     item.itemId= Date.now()
-    const sellerusername=JSON.parse(localStorage.currentuser)
+    // const sellerusername=JSON.parse(localStorage.currentuser)
+    const sellerusername="craftsworld_user"
     const seller= users.find(u=>u.username== sellerusername)
+    item.seller = {
+        username: seller.username,   
+        companyName: seller.companyName
+    }
+    item.image = localStorage.getItem('uploadedImage')
     items.push(item)
     localStorage.items=JSON.stringify(items)
 }
-
 function formToObject(form){
     const formData= new FormData(form)
     const data={}
@@ -23,4 +31,14 @@ function formToObject(form){
         data[key] =value
     }
     return data
+}
+uploadImage.addEventListener('change', function() {getImageAsUrl(this)})
+
+function getImageAsUrl(element) {
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        localStorage.setItem('uploadedImage', reader.result); //Store image
+    }
+    reader.readAsDataURL(file);
 }
