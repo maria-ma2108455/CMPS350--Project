ebcae1a3457a0d1d00f521c6bf465f7c566623ba
AishaAQ <101647309+AishaAQ@users.noreply.github.com>
2024-03-30 06:04:50 +0300

    the display upload images work but need to add popup

--- Main Page/js/addItem.js
+++ Main Page/js/addItem.js
@@ -1,5 +1,4 @@
 const urlParameter = new URLSearchParams(window.location.search);
-const uploadImage= document.querySelector('#item-image')
 const itemId = urlParameter.get('item');
 
 items= JSON.parse(localStorage.items)
@@ -7,19 +6,36 @@ users= JSON.parse(localStorage.users)
 const form = document.querySelector('#addItem-form')
 const cancelBTN = document.querySelector('#cancel-btn')
 const submitBTN = document.querySelector('#addItem-btn')
-const imageInputs = document.querySelector('#image-inputs')
+const fileInput = document.querySelector('#image');
+const preview = document.querySelector("#preview");
+const reader = new FileReader()
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (itemId) {
-        submitBTN.textContent = 'Update Item'
-    }
-})
+if (itemId) {
+    const item = items.find(i => i.itemId === itemId)
+    submitBTN.textContent = 'Update Item'
+    preview.src = item.image
+    preview.classList.remove('hidden')
+}
 
 form.addEventListener('submit',addForm)
 function addForm(e){
     console.log("Form submitted");
     e.preventDefault()
     const item = formToObject(e.target)
+    console.log(item);
+    if (!localStorage.itemUploaded) {
+        Swal.fire({
+            title: 'No Image Uploaded!',
+            text: 'Please upload an image for your item',
+            icon: 'error',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#d65f83'
+          }).then((result) => {
+            if(result.value){
+             this.props.submitUser(this.state)
+           }
+         })
+    }
     console.log(item)
     let foundIndex = items.findIndex(i => i.itemId == item.itemId)
     let add = false
@@ -68,24 +84,41 @@ function formToObject(form){
     return data
 }
 
-uploadImage.addEventListener('change', function() {getImageAsUrl(this)})
-
-function getImageAsUrl(element) {
-    var file = element.files[0];
-    var reader = new FileReader();
-    reader.onloadend = function() {
-        localStorage.setItem('uploadedImage', reader.result); //Store image
-        imageInputs.innerHTML = imageInputs.innerHTML + `<img src=${localStorage.uploadedImage} alt="Item Image">` 
-    }
-    reader.readAsDataURL(file);
+function handleEvent(event) {
+  
+  if (event.type === "load") {
+    localStorage.setItem('uploadedImage', reader.result); //Store image
+    preview.src = reader.result;
+    preview.classList.remove('hidden')
+  }
 }
+
+function addListeners(reader) {
+  reader.addEventListener("loadstart", handleEvent);
+  reader.addEventListener("load", handleEvent);
+  reader.addEventListener("loadend", handleEvent);
+  reader.addEventListener("progress", handleEvent);
+  reader.addEventListener("error", handleEvent);
+  reader.addEventListener("abort", handleEvent);
+}
+
+function handleSelected(e) {
+  
+  const selectedFile = fileInput.files[0];
+  if (selectedFile) {
+    addListeners(reader);
+    reader.readAsDataURL(selectedFile);
+  }
+}
+
+fileInput.addEventListener("change", handleSelected);
 
 if(urlParameter.get('item')){
     const itemid = urlParameter.get('item');
     const item = items.find(b=> b.itemId==itemid)
     const itemId= document.querySelector('#itemId')
     const name= document.querySelector('#item-name')
-    const image= document.querySelector('#item-image')
+    const image= document.querySelector('#image')
     const category= document.querySelector('#Ceramics')
     const price= document.querySelector('#item-price')
     const quantity= document.querySelector('#item-quantity')
