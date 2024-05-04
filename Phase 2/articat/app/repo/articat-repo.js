e6d435eed1189c86839ce05ee1d62020950bb691
maria-ma2108455 <ma2108455@qu.edu.qa>
2024-05-04 14:12:38 +0300

    repoadded

---
+++ Phase 2/articat/app/repo/articat-repo.js
@@ -0,0 +1,24 @@
+const prisma = new PrismaClient()
+
+class articatRepo{
+
+    async getUser(username){
+
+    }
+    async getItems(category){
+
+    }
+    async getFeatured(){
+
+    }
+    async getSearchItems(searchValue){
+        const search = searchValue.toLowerCase();
+        // javacode:
+        // filteredItems = items.filter( i => i.quantity > 0 && i.name.toLowerCase().includes(search.toLowerCase()) 
+        // || i.seller.companyName.toLowerCase().includes(search.toLowerCase()))
+
+
+    }
+
+
+}
