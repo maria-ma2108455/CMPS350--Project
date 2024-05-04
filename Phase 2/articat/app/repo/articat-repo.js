b437f284ec23feb3f497984c04e84ee2bc136f59
maria-ma2108455 <ma2108455@qu.edu.qa>
2024-05-04 15:27:25 +0300

    Repo methods added
    get all users, get customer, get seller, getsearch, get purchasehsitory

--- Phase 2/articat/app/repo/articat-repo.js
+++ Phase 2/articat/app/repo/articat-repo.js
@@ -1,9 +1,25 @@
 // const prisma = new PrismaClient()
 
 class articatRepo{
-
-    async getUser(username){
-
+    async getAllUsers(){
+        return await prisma.user.findMany()
+    }
+    async getCustomer(username) {
+    return await prisma.customer.findUnique({
+        where: {
+            username: username},
+        include: {
+            user: true }
+    })}
+    async getSeller(username) {
+        return await prisma.customer.findUnique({
+            where: {
+                username: username},
+            include: {
+                user: true }
+        })}
+    async getAllItems(){
+        return await prisma.item.findMany()
     }
     async getItems(category){
 
@@ -12,13 +28,27 @@ class articatRepo{
 
     }
     async getSearchItems(searchValue){
-        // const search = searchValue.toLowerCase();
+        const search = searchValue.toLowerCase()
+        return await prisma.item.findMany({
+            where: {
+                quantity: {gt:0}, 
+                name: {contains: search, mode: 'insensitive' //not casesensitive
+                }}})
         // javacode:
         // filteredItems = items.filter( i => i.quantity > 0 && i.name.toLowerCase().includes(search.toLowerCase()) 
         // || i.seller.companyName.toLowerCase().includes(search.toLowerCase()))
-
-
     }
-
-
-}
+    async getPurchaseHistory(currentUser){
+        return await prisma.purchase.findMany({
+            where: {
+                customerUN: currentUser
+            },
+            include: {
+                item:{
+                    include: {
+                    seller: true 
+                }}},
+            orderBy: {
+                date: 'desc'}})
+            }
+        }
