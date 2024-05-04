37471b814da56f155c0b136304753914f518650f
AishaAQ <101647309+AishaAQ@users.noreply.github.com>
2024-05-05 02:10:57 +0300

    sale history

--- Phase 2/articat/app/repo/articat-repo.js
+++ Phase 2/articat/app/repo/articat-repo.js
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default class articatRepo{
-    async getAllUsers(){
+export default class articatRepo {
+    async getAllUsers() {
         return await prisma.user.findMany()
     }
-    async getUser(username){
+    async getUser(username) {
 
         return await prisma.user.findUnique({
             where: {
@@ -14,34 +14,39 @@ export default class articatRepo{
         })
     }
     async getCustomer(username) {
-    return await prisma.customer.findUnique({
-        where: {
-            username: username},
-        include: {
-            user: true }
-    })}
-   
+        return await prisma.customer.findUnique({
+            where: {
+                username: username
+            },
+            include: {
+                user: true
+            }
+        })
+    }
+
     async getSeller(username) {
         try {
             return await prisma.customer.findUnique({
                 where: {
-                    username: username},
+                    username: username
+                },
                 include: {
-                    user: true }
+                    user: true
+                }
             })
         } catch (error) {
             return { error: error.message }
         }
     }
-        
-    async getAllItems(){
+
+    async getAllItems() {
         try {
             return await prisma.item.findMany()
         } catch (error) {
             return { error: error.message }
         }
     }
-    async getItems(category){
+    async getItems(category) {
         try {
             if (category === 'all')
                 return prisma.item.findMany()
@@ -55,28 +60,31 @@ export default class articatRepo{
         }
     }
 
-    
-    async getFeatured(){
+
+    async getFeatured() {
         try {
             return await prisma.item.findMany({
                 where: {
-                    featured:true
+                    featured: true
                 }
             })
         } catch (error) {
             return { error: error.message }
         }
-       
+
     }
-    async getSearchItems(searchValue){
+    async getSearchItems(searchValue) {
         const search = searchValue.toLowerCase()
         try {
-           
+
             return await prisma.item.findMany({
-                where: { 
-                    quantity: {gt:0}, 
-                    name: {contains: search
-                    }}})
+                where: {
+                    quantity: { gt: 0 },
+                    name: {
+                        contains: search
+                    }
+                }
+            })
         } catch (error) {
             return { error: error.message }
         }
@@ -84,33 +92,70 @@ export default class articatRepo{
         // filteredItems = items.filter( i => i.quantity > 0 && i.name.toLowerCase().includes(search.toLowerCase()) 
         // || i.seller.companyName.toLowerCase().includes(search.toLowerCase()))
     }
-    async getPurchaseHistory(currentUser){
+
+    async getHistory(username) {
+        const currentUser = await this.getUser(username)
+        if (currentUser.type === 'customer') return this.getPurchaseHistory(username)
+        else return this.getSaleHistory(username)
+    }
+
+    async getPurchaseHistory(currentUser) {
         try {
-            return await prisma.purchase.findMany({
-                where: {
-                    customerUN: currentUser
-                },
-                include: {
-                    item:{
-                        include: {
-                        seller: true 
-                    }}},
-                orderBy: {
-                    date: 'desc'}})
-                }
-         catch (error) {
+            return {
+                purchases: await prisma.purchase.findMany({
+                    where: {
+                        customerUN: currentUser
+                    },
+                    include: {
+                        item: {
+                            include: {
+                                seller: true
+                            }
+                        }
+                    },
+                    orderBy: {
+                        date: 'desc'
+                    }
+                })
+            }
+        }
+        catch (error) {
             return { error: error.message }
         }
+    }
+
+    async getSaleHistory(seller) {
+        try {
+            return {
+                sales: await prisma.purchase.findMany({
+                    where: {
+                        item: {
+                            sellerUN: seller
+                        }
+                    },
+                    include: {
+                        item: true,
+                        customer: true
+                    },
+                    orderBy: {
+                        date: 'desc'
+                    }
+                })
+            }
         }
+        catch (error) {
+            return { error: error.message }
+        }
+    }
 
     async addItem(item) {
         try {
             return prisma.item.create({
                 data: item
-             })
+            })
         } catch (error) {
             return { error: error.message }
-            }
+        }
     }
 
     async getItem(itemId) {
@@ -133,5 +178,5 @@ export default class articatRepo{
             return { error: error.message }
         }
     }
-            
-    }
+
+}
