fc36c6b901071457b1895562644227482dca5d58
maria-ma2108455 <ma2108455@qu.edu.qa>
2024-05-08 20:45:09 +0300

    header removal

--- Phase 2/articat/app/api/items/[itemid]/route.js
+++ Phase 2/articat/app/api/items/[itemid]/route.js
@@ -4,16 +4,9 @@ const ArtiCatRepo = new articatRepo()
 export async function GET(request, { params }) {
     const itemId= params.itemid
     const item = await ArtiCatRepo.getItem(itemId)
-    const headers = new Headers({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',  
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',  
-    })
 
     return  Response.json(item, {
-        status: 200,
-        headers: headers
+        status: 200
     })
 
 }
