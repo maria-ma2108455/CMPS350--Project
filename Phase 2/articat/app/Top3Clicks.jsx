eb655c41244775783ab2785f0bffc8dc853b85b4
mais-ma2103676 <157325009+mais-ma2103676@users.noreply.github.com>
2024-05-10 02:58:43 +0300

    oh la la responsive

--- Phase 2/articat/app/Top3Clicks.jsx
+++ Phase 2/articat/app/Top3Clicks.jsx
@@ -1,49 +1,61 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 import styles from "./page.module.css";
 
-export default function Top3Chart({top3Clicks}) {
+export default function Top3Chart({ top3Clicks }) {
   const [items, setItems] = useState([]);
 
   //use staate and use effect to avoid hydration errors
-    if (!top3Clicks) {
-        return <p>....</p>
-      }
-      useEffect(() => {
-        if (top3Clicks) {
-          setItems(top3Clicks.map(item => ({
-            name: item.name,
-            "Number Of Clicks": item.clicks,
-          }))
-        )
-        }
-    }, [top3Clicks])
-    
-        return (
-            <>
-             <div className={styles.barChart}>
-          <BarChart
-          
-            width={650}
-            height={400}
-            data={items}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-            barSize={70}
-          >
-            <XAxis dataKey="name" />
-            <YAxis/>
-            <Tooltip />
-            <Legend />
-            <CartesianGrid strokeDasharray="2 1" />
-            <Bar dataKey="Number Of Clicks" fill="#d65f83" background={{ fill: '#eee' }} />
-          </BarChart>
-          </div>
-          </>
-        );
-      }
+  if (!top3Clicks) {
+    return <p>....</p>;
+  }
+  useEffect(() => {
+    if (top3Clicks) {
+      setItems(
+        top3Clicks.map((item) => ({
+          name: item.name,
+          "Number Of Clicks": item.clicks,
+        }))
+      );
+    }
+  }, [top3Clicks]);
+
+  return (
+
+    <ResponsiveContainer width="95%" height={400}>
+      <BarChart
+        width={650}
+        height={400}
+        data={items}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+        barSize={70}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray="2 1" />
+        <Bar
+          dataKey="Number Of Clicks"
+          fill="#d65f83"
+          background={{ fill: "#eee" }}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
