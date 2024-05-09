4579d31ee00e9d202162b47e04da4d73c748df8a
mais-ma2103676 <157325009+mais-ma2103676@users.noreply.github.com>
2024-05-09 12:49:23 +0300

    dumb mistake

--- Phase 2/articat/app/api/[userid]/route.js
+++ Phase 2/articat/app/api/[userid]/route.js
@@ -16,7 +16,7 @@ export async function GET(request, { params }) {
 }
 
 export async function PUT(request, { params }){
-    const username= params.userId
+    const username= params.userid
     const customerUpdate = await request.json()
     const updatedCustomer = await ArtiCatRepo.updateCustomer(username,customerUpdate)
     return Response.json(updatedCustomer, { status: 200 })
