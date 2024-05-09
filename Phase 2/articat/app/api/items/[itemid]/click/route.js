2508f43f4d3634526f4f2d799d3085920897c361
mais-ma2103676 <157325009+mais-ma2103676@users.noreply.github.com>
2024-05-09 22:43:15 +0300

    just skeleton code

---
+++ Phase 2/articat/app/api/items/[itemid]/click/route.js
@@ -0,0 +1,9 @@
+import articatRepo from "@/app/repo/articat-repo"
+const ArtiCatRepo = new articatRepo()
+
+export async function PUT(request, { params }){
+    // const itemId= params.itemid
+    // const itemUpdate = await request.json()
+    // const updatedItems = await ArtiCatRepo.updateItemClick(itemId,itemUpdate)
+    // return Response.json(updatedItems, { status: 200 })
+}
