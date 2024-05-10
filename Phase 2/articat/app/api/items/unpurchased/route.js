8f8fb6072afcee066acb2a5ce983dcde00d5beff
AishaAQ <101647309+AishaAQ@users.noreply.github.com>
2024-05-10 23:40:13 +0300

    cleaning up

--- Phase 2/articat/app/api/items/unpurchased/route.js
+++
@@ -1,7 +0,0 @@
-import articatRepo from "@/app/repo/articat-repo"
-const AtriCatRepo = new articatRepo()
-
-export async function GET(request) {
-    const unPurchased = await AtriCatRepo.getUnpurchasedProducts()
-    return Response.json(unPurchased)
-}
